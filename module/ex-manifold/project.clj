206e1aead3e958a813589af6b56b005dab917c03
Max Penet <max.penet@exoscale.ch>
2019-10-08 17:08:29 +0200

    make sure we test against latest

--- module/ex-manifold/project.clj
+++ module/ex-manifold/project.clj
@@ -1,6 +1,6 @@
 (load-file "../../setup.clj")
 (defproject exoscale/ex-manifold (:project/version properties)
-  :profiles {:dev {:dependencies [[exoscale/ex "0.1.0-SNAPSHOT"]
+  :profiles {:dev {:dependencies [[exoscale/ex ~(:project/version properties)]
                                   [manifold "0.1.8"]]}}
   :url "https://github.com/exoscale/ex"
   :source-paths ["src/clj"])
