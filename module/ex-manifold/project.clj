8b3e9286c861bc13851a4f4d8c058045a2436d52
Max Penet <max.penet@exoscale.ch>
2019-10-08 17:07:24 +0200

    resource path is useless

--- module/ex-manifold/project.clj
+++ module/ex-manifold/project.clj
@@ -1,6 +1,5 @@
 (load-file "../../setup.clj")
 (defproject exoscale/ex-manifold (:project/version properties)
-  :resource-paths ["../../resources"]
   :profiles {:dev {:dependencies [[exoscale/ex "0.1.0-SNAPSHOT"]
                                   [manifold "0.1.8"]]}}
   :url "https://github.com/exoscale/ex"
