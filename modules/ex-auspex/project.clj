f4742a5888ddb5df9b8140403f139e75428ec3fd
Max Penet <max.penet@exoscale.ch>
2019-10-09 10:28:52 +0200

    auspex should be a dep

--- modules/ex-auspex/project.clj
+++ modules/ex-auspex/project.clj
@@ -1,6 +1,6 @@
 (load-file "../../.setup.clj")
 (defproject exoscale/ex-auspex (:project/version properties)
-  :profiles {:dev {:dependencies [[exoscale/ex ~(:project/version properties)]
-                                  [cc.qbits/auspex "0.1.0-alpha2"]]}}
+  :dependencies [[cc.qbits/auspex "0.1.0-alpha2"]]
+  :profiles {:dev {:dependencies [[exoscale/ex ~(:project/version properties)]]}}
   :url "https://github.com/exoscale/ex"
   :source-paths ["src/clj"])
