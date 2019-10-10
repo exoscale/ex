015dc3fae9fe11085bd46838cf873943e7a9867e
Max Penet <max.penet@exoscale.ch>
2019-10-10 16:44:59 +0200

    Version 0.1.1

--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.1.1-SNAPSHOT"
+(defproject exoscale/ex "0.1.1"
   :dependencies [[org.clojure/clojure ]]
   :plugins [[lein-modules "0.3.11"]]
   :modules {:parent "../../"})
