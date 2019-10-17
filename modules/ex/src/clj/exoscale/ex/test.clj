3b83b81cf9218f0e4a82464bf3f28277611033f9
Max Penet <max.penet@exoscale.ch>
2019-10-17 13:37:09 +0200

    Add missing test file

---
+++ modules/ex/src/clj/exoscale/ex/test.clj
@@ -0,0 +1,28 @@
+(ns exoscale.ex.test
+  "Testing utilities"
+  (:require [clojure.test :as t]
+            [exoscale.ex :as ex]))
+
+(defmethod t/assert-expr 'thrown-ex-data? [msg form]
+  ;; (is (thrown-ex-data? c expr))
+  ;; Asserts that evaluating expr throws an exceptioninfo of :type `k`.
+  ;; Returns the exceptioninfo thrown.
+  (let [k (nth form 1)
+        body (nthnext form 2)]
+    `(ex/try+
+       ~@body
+       (t/do-report {:type :fail
+                     :message ~msg
+                     :expected '~form
+                     :actual nil})
+       (catch-data ~k d#
+                   (t/do-report {:type :pass
+                                 :message ~msg,
+                                 :expected '~form
+                                 :actual d#})
+                   (::ex/exception d#))
+       (catch Exception e#
+         (t/do-report {:type :fail
+                       :message ~msg
+                       :expected '~form
+                       :actual e#})))))
