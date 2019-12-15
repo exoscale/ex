0966a493763371ab30664f6f1ea6ea3c72794ac3
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-12-15 11:59:00 +0100

    Version 0.3.6-SNAPSHOT

--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.3.5"
+(defproject exoscale/ex "0.3.6-SNAPSHOT"
   :dependencies [[org.clojure/clojure ]]
   :plugins [[lein-modules "0.3.11"]]
   :modules {:parent "../../"})
