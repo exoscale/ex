ce4ec674f8b54a792563c5c0c922e534170efb08
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-05-22 11:43:10 +0200

    Version 0.3.9-SNAPSHOT

--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.3.8"
+(defproject exoscale/ex "0.3.9-SNAPSHOT"
   :plugins [[lein-parent "0.3.8"]]
   :source-paths ["src/clj"]
   :parent-project {:path "../../project.clj"
