529b5fdecad10aadaa967313718d3f51260d4a3e
Max Penet <max.penet@exoscale.ch>
2020-07-06 17:01:23 +0200

    Version 0.3.10

--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.3.10-SNAPSHOT"
+(defproject exoscale/ex "0.3.10"
   :plugins [[lein-parent "0.3.8"]]
   :source-paths ["src/clj"]
   :parent-project {:path "../../project.clj"
