dd495844baf04d972212395759d35fe575af935c
Wouter Dullaert <wouter.dullaert@exoscale.ch>
2020-07-24 18:03:54 +0200

    Version 0.3.13-SNAPSHOT

--- modules/ex-manifold/project.clj
+++ modules/ex-manifold/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex-manifold "0.3.12"
+(defproject exoscale/ex-manifold "0.3.13-SNAPSHOT"
   :plugins [[lein-parent "0.3.8"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:managed-dependencies
--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.3.12"
+(defproject exoscale/ex "0.3.13-SNAPSHOT"
   :plugins [[lein-parent "0.3.8"]]
   :source-paths ["src/clj"]
   :parent-project {:path "../../project.clj"
