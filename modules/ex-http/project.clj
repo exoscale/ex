a5c6c080fc0980926e3dd52a95fddbb7c5614564
Wouter Dullaert <wouter.dullaert@exoscale.ch>
2020-07-30 12:10:54 +0200

    Version 0.3.13

--- modules/ex-http/project.clj
+++ modules/ex-http/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex-http "0.3.13-SNAPSHOT"
+(defproject exoscale/ex-http "0.3.13"
   :plugins [[lein-parent "0.3.8"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:managed-dependencies
