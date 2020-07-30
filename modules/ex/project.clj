00a9f326b290e8802b43c92ab4a1736794709de6
Wouter Dullaert <wouter.dullaert@exoscale.ch>
2020-07-30 12:11:52 +0200

    Version 0.3.14-SNAPSHOT

--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.3.13"
+(defproject exoscale/ex "0.3.14-SNAPSHOT"
   :plugins [[lein-parent "0.3.8"]]
   :source-paths ["src/clj"]
   :parent-project {:path "../../project.clj"
