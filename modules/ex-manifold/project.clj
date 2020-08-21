50f63446d97f736e6b7f3a9cceb7ea7c430c8363
Max Penet <max.penet@exoscale.ch>
2020-08-21 08:28:28 +0200

    Version 0.3.15-SNAPSHOT

--- modules/ex-manifold/project.clj
+++ modules/ex-manifold/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex-manifold "0.3.14"
+(defproject exoscale/ex-manifold "0.3.15-SNAPSHOT"
   :plugins [[lein-parent "0.3.8"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:managed-dependencies
