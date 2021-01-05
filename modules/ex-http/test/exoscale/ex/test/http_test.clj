cf73f11a0b73e371ebca86dc08b76c9ab5a66e97
Max Penet <max.penet@exoscale.ch>
2021-01-05 09:51:11 +0100

    change ex-http ex-info format (#23)
    * make the http ex more specific and set the response at the root of the
    ex-data
    
    This is backward compatible

--- modules/ex-http/test/exoscale/ex/test/http_test.clj
+++ modules/ex-http/test/exoscale/ex/test/http_test.clj
@@ -32,6 +32,9 @@
   (testing "Should return ex/fault as the default option"
     (is (thrown-ex-info-type? ::ex/fault (ex-http/response->ex-info! {:status :not-mapped}))))
 
+  (is (thrown-ex-info-type? ::ex-http/response (ex-http/response->ex-info! {:status :not-mapped}))
+      "They all derive from top level ::ex-http/response")
+
   (testing "Should return ex/not-found for a 404"
     (is (thrown-ex-info-type? ::ex/not-found (ex-http/response->ex-info! {:status 404}))))
 
@@ -39,4 +42,3 @@
     (do (defmethod ex-http/response->ex-info! 404 [_] ::overwritten)
         (is (= ::overwritten
                (ex-http/response->ex-info! {:status 404}))))))
-
