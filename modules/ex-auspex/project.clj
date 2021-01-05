06d79d14a6bbfd73ec3832cc343aeac8cf5b126d
Max Penet <max.penet@exoscale.ch>
2021-01-05 09:52:36 +0100

    Version 0.3.17

--- modules/ex-auspex/project.clj
+++ modules/ex-auspex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex-auspex "0.3.17-SNAPSHOT"
+(defproject exoscale/ex-auspex "0.3.17"
   :plugins [[lein-parent "0.3.8"]]
   :parent-project {:path "../../project.clj"
                    :inherit [:managed-dependencies
--- modules/ex/project.clj
+++ modules/ex/project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ex "0.3.17-SNAPSHOT"
+(defproject exoscale/ex "0.3.17"
   :plugins [[lein-parent "0.3.8"]]
   :source-paths ["src/clj"]
   :parent-project {:path "../../project.clj"
