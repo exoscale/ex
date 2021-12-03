2e04cea0b0d02a736ba99ab922b465c47f125172
Max Penet <max.penet@exoscale.ch>
2021-12-03 11:00:11 +0100

    add thrown-with-ex-info-msg and thrown-with-ex-info-cause

--- modules/ex/src/clj/exoscale/ex/test.clj
+++ modules/ex/src/clj/exoscale/ex/test.clj
@@ -26,3 +26,35 @@
                       :message ~msg
                       :expected '~form
                       :actual e#})))))
+
+(defmethod t/assert-expr 'thrown-with-ex-info-msg? [msg form]
+  (let [k (nth form 1)
+        re (nth form 2)
+        body (nthnext form 3)]
+    `(ex/try+ ~@body
+              (do-report {:type :fail, :message ~msg, :expected '~form, :actual nil})
+              (catch ~k d#
+                (let [e# (:exoscale.ex/exception d#)
+                      m# (ex-message e#)]
+                  (if (re-find ~re m#)
+                    (do-report {:type :pass, :message ~msg,
+                                :expected '~form, :actual e#})
+                    (do-report {:type :fail, :message ~msg,
+                                :expected '~form, :actual e#})))
+                e#))))
+
+(defmethod t/assert-expr 'thrown-with-ex-info-cause? [msg form]
+  (let [k (nth form 1)
+        re (nth form 2)
+        body (nthnext form 3)]
+    `(ex/try+ ~@body
+              (do-report {:type :fail, :message ~msg, :expected '~form, :actual nil})
+              (catch ~k d#
+                (let [e# (:exoscale.ex/exception d#)
+                      m# (ex-cause e#)]
+                  (if (re-find ~re m#)
+                    (do-report {:type :pass, :message ~msg,
+                                :expected '~form, :actual e#})
+                    (do-report {:type :fail, :message ~msg,
+                                :expected '~form, :actual e#})))
+                e#))))
